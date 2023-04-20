87acbe73e91486b04ae7f16352065fb90f3a0e38
jwloqwe <chanjunwei2000@gmail.com>
2023-04-21 00:22:07 +0800

    Added a new set of codes for library

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
-const fs = require("fs/promises")
-const express = require("express")
-const cors = require("cors")
-const _ = require("lodash")
-const { v4: uuid } = require("uuid")
+const express = require("express");
+const sqlite3 = require('sqlite3').verbose();
+const openai = require('openai');
 
 const app = express();
+const port = 3000;
 
-app.get("/outfit", (req, res) => {
-    res.send("This is working!!!");
+// Configure OpenAI credentials
+openai.apiKey = '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';
+
+// Configure SQLite database
+const db = new sqlite3.Database(':memory:');
+
+// Create table for book summaries
+db.serialize(() => {
+    db.run('CREATE TABLE summaries (id INTEGER PRIMARY KEY, title TEXT, summary TEXT)');
 });
 
-app.listen(3000, () => console.log("API Server is running..."));
+// Route for getting book summaries
+app.get('/summary', (req, res) => {
+    const title = req.query.title;
+
+    // Generate book summary using OpenAI
+    openai.completions.create({
+            engine: 'davinci',
+            prompt: `Generate a summary for the book "${title}".`,
+            maxTokens: 256,
+            n: 1,
+            stop: '\n',
+        })
+        .then((response) => {
+            const summary = response.choices[0].text.trim();
+
+            // Store summary in database
+            db.run('INSERT INTO summaries (title, summary) VALUES (?, ?)', [title, summary], (err) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('Error storing summary in database.');
+                } else {
+                    res.json({ title, summary });
+                }
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error generating book summary.');
+        });
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
