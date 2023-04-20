f646d31c37d4cc65e283e95978b4049daecb902e
jwloqwe <chanjunwei2000@gmail.com>
2023-04-20 11:21:55 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require("fs/promises")
+const express = require("express")
+const cors = require("cors")
+const _ = require("lodash")
+const { v4: uuid } = require("uuid")
+
+const app =express();
+
+app.get("/outfit", (req, res) =>{
+    res.send("This is working!!!");
+});
+
+app.listen(3000,() => console.log("API Server is running..."));
